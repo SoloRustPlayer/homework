e75b2e7557504918cc73d4b6ae732e97ba808fd6
User <User@SAVELIY-PC>
2025-02-08 00:50:24 +0200

    Add methods for displaying, resizing, and sorting Array class

--- homework/Array.h
+++ homework/Array.h
@@ -27,4 +27,60 @@ public:
     ~Array() {
         delete[] data;
     }
+    void display() const {
+        for (size_t i = 0; i < size; i++) {
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void fill(int min, int max) {
+        for (size_t i = 0; i < size; i++) {
+            data[i] = min + (i % (max - min + 1));
+        }
+    }
+
+    void resize(size_t newSize) {
+        int* newData = new int[newSize] {};
+        size_t minSize = (newSize < size) ? newSize : size;
+        for (size_t i = 0; i < minSize; i++) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+        size = newSize;
+    }
+
+    void sort() {
+        for (size_t i = 0; i < size - 1; i++) {
+            for (size_t j = 0; j < size - i - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    int min() const {
+        int minValue = data[0];
+        for (size_t i = 1; i < size; i++) {
+            if (data[i] < minValue) {
+                minValue = data[i];
+            }
+        }
+        return minValue;
+    }
+
+    int max() const {
+        int maxValue = data[0];
+        for (size_t i = 1; i < size; i++) {
+            if (data[i] > maxValue) {
+                maxValue = data[i];
+            }
+        }
+        return maxValue;
+    }
+
 };
